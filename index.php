f7c90282a1888251786a8e2bdfda23acc141dffa
Eric JS <esadowski@gmail.com>
2021-05-01 19:28:34 +0000

    file and directory setup

--- index.php
+++ index.php
@@ -1,64 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Wake Up</title>
-        <style>
-            body {
-                background-color: #191919;
-                background-image: radial-gradient(#222 40%, transparent 60%);
-                background-size: 5px 5px;
-                margin: 0;
-                padding: 0;
-            }
-            main {
-                height: 100vh;
-                position: relative;
-            }/*
-            main::before {
-                content: "";
-                display: block;
-                margin: 0;
-                padding: 0;
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                background-image: radial-gradient(at 35% -20%, rgba(255,255,255,0.1) 0%, transparent 80%);
-                background-size: 160%, 100%;
-                z-index: -1;
-            }
-            main::after {
-                content: "";
-                display: block;
-                margin: 0;
-                padding: 0;
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                background-image: radial-gradient(at 25% -20%, rgba(255,255,255,0.1) 0%, transparent 80%);
-                background-size: 150%, 100%;
-                z-index: -1;
-            }*/
-            #container {
-                position: absolute;
-                width: 100%;
-                top: 50%;
-                transform: translateY(-50%);
-            }
-            h1 {
-                color: #999;
-                text-shadow: 1px 3px 5px rgba(0,0,0,0.5);
-                text-align: center;
-                font-size: 3vmin;
-                margin: 0;
-                padding: 0 5rem;
-                font-family: Verdana, Geneva, Tahoma, sans-serif;
-                font-weight: normal;
-            }
-        </style>
+        <title>Projects &rtrif; Eric J.S.</title>
+        <link rel="stylesheet" href="https://unpkg.com/sanitize.css">
+        <link rel="stylesheet" href="styles/main.css">
     </head>
     <body>
         <main>
@@ -67,22 +12,4 @@
             </div>
         </main>
     </body>
-    <script>
-        let quotes = [
-            "You are not happy here.",
-            "You still live with your parents.",
-            "Stop procrastinating.",
-            "Put the work in to create the future you want.",
-            "You have goals, now work towards them.",
-            "Be productive.",
-            "Pick something on your to-do list and do it.",
-            "Impress yourself.",
-            "This low point will pass."
-        ];
-        //console.log(quotes);
-        let index = Math.floor(Math.random() * (quotes.length));
-        //console.log(index);
-        //console.log(quotes[index]);
-        //document.getElementById("displayText").innerHTML = quotes[index];
-    </script>
 </html>
