c269e5209c8cd31d1df9c5ccda8b4ef500a07fae
Eric JS <esadowski@gmail.com>
2021-05-14 01:23:37 +0000

    started contact page

---
+++ contact/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contact &rtrif; Eric J.S.</title>
+        <?php include '../head-links.php' ?>
+    </head>
+    <body>
+        <!-- index -->
+        <div id="body-container">
+            <?php include '../logo-header.php' ?>
+            <main id="contact">
+                <section id="contact-iframe-container">
+                    <iframe class="contact-iframe" src="https://docs.google.com/forms/d/e/1FAIpQLScQCbilO3f1kGRLXYdxlfSd01cmPnlVBApTjBp4cTYSYuzZbA/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">Loading…</iframe>
+                </section>
+            </main>
+            <?php include '../nav.php' ?>
+        </div>
+    </body>
+</html>
