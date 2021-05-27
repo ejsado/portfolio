53a7f386b6a8572c770547cfdded4ed5dd506bbc
Eric JS <esadowski@gmail.com>
2021-05-27 13:47:02 +0000

    small changes to instagram embed and about page

--- adventures/adventures.php
+++ adventures/adventures.php
@@ -1,4 +1,6 @@
 <!-- adventure content -->
 <main id="adventures">
-    <div class="embedsocial-hashtag" data-ref="8d2eedac0532aa682678823cda1251dce3d2a820" ><a class="feed-powered-by-es" href="https://embedsocial.com/products/embedfeed/" target="_blank" title="Powered by EmbedSocial">Powered by EmbedSocial<span>→</span></a></div><script>(function(d, s, id){var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/cdn/ht.js"; d.getElementsByTagName("head")[0].appendChild(js);}(document, "script", "EmbedSocialHashtagScript"));</script>
+    <section id="instagram-embed">
+        <div class="embedsocial-hashtag" data-ref="8d2eedac0532aa682678823cda1251dce3d2a820" ><a class="feed-powered-by-es" href="https://embedsocial.com/products/embedfeed/" target="_blank" title="Powered by EmbedSocial">Powered by EmbedSocial<span>→</span></a></div><script>(function(d, s, id){var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/cdn/ht.js"; d.getElementsByTagName("head")[0].appendChild(js);}(document, "script", "EmbedSocialHashtagScript"));</script>
+    </section>
 </main>
