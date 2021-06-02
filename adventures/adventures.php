f61392ae861c717ce0f2b2a7b33a345ec296f9ec
Eric JS <esadowski@gmail.com>
2021-06-02 00:57:07 +0000

    added adventure gallery

--- adventures/adventures.php
+++ adventures/adventures.php
@@ -1,6 +1,28 @@
 <!-- adventure content -->
 <main id="adventures">
-    <section>
-        
+    <section id="instagram-highlights">
+        <?php
+            $photosPath = '/media/small-photos/';
+            // get an array of files and directories as strings
+            $directoryContents = scandir('../media/small-photos');
+            //print_r($directoryContents);
+
+
+            // randomize image order
+            shuffle($directoryContents);
+
+            foreach($directoryContents as $imageRef) {
+                if($imageRef != "." && $imageRef != "..") {
+                    echo '<div class="photo-container" style="background-image: url(\'' . $photosPath . $imageRef . '\')">
+                        <img class="photo-image" src="' . $photosPath . $imageRef . '" alt="Instagram highlight" loading="lazy">
+                    </div>';
+                }
+            }
+        ?>
+        <!--
+        <div class="photo-container">
+            <img class="photo-image" src="" alt="Instagram highlight" loading="lazy">
+        </div>
+        -->
     </section>
 </main>
