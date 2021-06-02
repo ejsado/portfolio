da8c3be0db90c07ddb6aa2321d6a23864e9f9dcf
Eric JS <esadowski@gmail.com>
2021-06-02 16:43:52 +0000

    finished adventures page

--- adventures/adventures.php
+++ adventures/adventures.php
@@ -1,5 +1,22 @@
 <!-- adventure content -->
 <main id="adventures">
+    <section>
+        <div id="instagram-badge">
+            <div>
+                <a href="https://www.instagram.com/maryunleashed/">
+                    <img src="/media/instagram-glyph.png" alt="Instagram logo">
+                </a>
+            </div>
+            <div>
+                <p>My Travel Photojournal</p>
+                <p>I use Instagram to log all of my adventures, including road trips, vacations, and outdoor activities.</p>
+                <p>
+                    <a href="https://www.instagram.com/maryunleashed/">Check it out &rarr;</a>
+                </p>
+            </div>
+        </div>
+        <h3>Here are some highlights...</h3>
+    </section>
     <section id="instagram-highlights">
         <?php
             $photosPath = '/media/small-photos/';
