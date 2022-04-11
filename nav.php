12d26f904e9393fdb7603ccbf512a6b1cb7bf788
Eric JS <esadowski@gmail.com>
2022-04-11 16:09:11 +0000

    reorder nav links

--- nav.php
+++ nav.php
@@ -1,19 +1,19 @@
 <!-- navigation content -->
 <nav>
-    <a class="pill-button 
-        <?php if ($currentPage == "about") {echo "current-page";} ?>"
-        href="/about/">
-        About
-    </a>
-    <a class="pill-button 
+	<a class="pill-button 
         <?php if ($currentPage == "projects") {echo "current-page";} ?>"
         href="/">
         Projects
     </a>
-    <a class="pill-button 
+	<a class="pill-button 
         <?php if ($currentPage == "adventures") {echo "current-page";} ?>"
         href="/adventures/">
         Adventures
+    </a>
+    <a class="pill-button 
+        <?php if ($currentPage == "about") {echo "current-page";} ?>"
+        href="/about/">
+        About
     </a>
 	<!-- <a class="pill-button 
         <?php if ($currentPage == "other") {echo "current-page";} ?>"
