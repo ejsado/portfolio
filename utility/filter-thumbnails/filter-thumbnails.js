687084030536303b58921349e34bdcf11ae6a690
ejsado <ejsado@users.noreply.github.com>
2022-05-16 15:56:23 -0400

    adjust filter logic

--- utility/filter-thumbnails/filter-thumbnails.js
+++ utility/filter-thumbnails/filter-thumbnails.js
@@ -17,8 +17,12 @@ function getFilterNameFromId(elementIdString) {
 	return elementIdString.slice(7);
 }
 
-// return true if any class names are found in tokenList
+// return true if ANY ONE of these class names are found in tokenList
 function containsOneOfManyClasses(classArray, tokenList) {
+	// no required filters are set, this is accepted
+	if (classArray.length == 0) {
+		return true;
+	}
 	for (const className of classArray) {
 		if (tokenList.contains(className)) {
 			return true;
@@ -27,18 +31,38 @@ function containsOneOfManyClasses(classArray, tokenList) {
 	return false;
 }
 
-// iterate through filter checkboxes
-// get class names of filters
-// add 'filter-hide' class to thumbnails not containing filter classes
-function updateFilter() {
+// return true if ALL class names are found in tokenList
+function containsAllClasses(classArray, tokenList) {
+	// no required filters are set, this is accepted
+	if (classArray.length == 0) {
+		return true;
+	}
+	for (const className of classArray) {
+		if (!tokenList.contains(className)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// create an array of class names from checkboxes that are checked
+function classNameArray(checkboxGroup) {
 	let filterShow = [];
-	for (let checkbox of filterCheckboxes) {
+	for (let checkbox of checkboxGroup) {
 		if (checkbox.checked == true) {
 			filterShow.push(getFilterNameFromId(checkbox.id));
 		}
 	}
+	return filterShow;
+}
+
+// iterate through filter checkboxes
+// get class names of filters
+// add 'filter-hide' class to thumbnails not containing filter classes
+function updateFilter(allArray, anyArray) {
 	for (let thumbnail of projectThumbnails) {
-		if (containsOneOfManyClasses(filterShow, thumbnail.classList)) {
+		if (containsAllClasses(allArray, thumbnail.classList) && 
+			containsOneOfManyClasses(anyArray, thumbnail.classList)) {
 			thumbnail.classList.remove("filter-hide");
 		} else {
 			thumbnail.classList.add("filter-hide");
@@ -64,6 +88,16 @@ function clearFilters() {
 	collapseButton.classList.remove("filters-active");
 }
 
+// only allow one category to be selected
+function deselectOthers(checkbox, selectedGroup) {
+	const checkboxGroup = document.getElementsByName(selectedGroup);
+	for (let element of checkboxGroup) {
+		if (element.id != checkbox.id && element.type == "checkbox") {
+			element.checked = false;
+		}
+	}
+}
+
 
 // create an array with all the available filters
 const filterCheckboxes = document.getElementsByClassName('filter-checkbox');
@@ -74,6 +108,12 @@ const projectThumbnails = document.getElementsByClassName("project-thumbnail");
 // get the filter collapse button
 const collapseButton = document.getElementById('collapse-label');
 
+// filters operating with AND
+const filterAnd = document.getElementsByName("category");
+
+// filters operating with OR
+const filterOr = document.getElementsByName("tool");
+
 // add event listeners for checkbox changes
 for (let checkbox of filterCheckboxes) {
 	checkbox.addEventListener('change', (event) => {
@@ -81,7 +121,10 @@ for (let checkbox of filterCheckboxes) {
 		if (filtersActive()) {
 			// add star from filters button
 			collapseButton.classList.add("filters-active");
-			updateFilter();
+			if (event.currentTarget.name == "category") {
+				deselectOthers(event.currentTarget, event.currentTarget.name);
+			}
+			updateFilter(classNameArray(filterAnd), classNameArray(filterOr));
 		} else {
 			// else display all thumbnails
 			// remove star from filters button
